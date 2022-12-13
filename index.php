cf295a0bcff69da3058f0a04b54c332fe8c8f871
Ruan Serra Azul <ruan.4321.pablo@gmail.com>
2022-12-13 10:30:34 -0300

    mudada estrutura de projeto

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include('header.php'); ?>
+<?php include('./layouts/header.php'); ?>
 <body>
 
 
@@ -117,7 +117,7 @@
 	</div>
 	
 	</div>
-	<?php include('footer.php'); ?>
+	<?php include('./layouts/footer.php'); ?>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
